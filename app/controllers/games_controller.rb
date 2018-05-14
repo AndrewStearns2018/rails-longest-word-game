f95f241f77d058356cb4e3e1577ed0d1651a9e89
Andrew Stearns <andrew.stearns@sciencespo.fr>
2018-05-14 16:50:53 +0200

    we have a working new method and a score form

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,13 @@
+class GamesController < ApplicationController
+  def new
+    alphabet = ("A".. "Z").to_a
+    @letters = []
+    while @letters.length < 10
+      @letters.push(alphabet.sample)
+    end
+    @letters
+  end
+
+  def score
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'new', to: 'games#new', as: :new
+  post 'score', to: 'games#score'
+  # I'm not quite sure why this needs to be a post here.
+  # In the previous problem, we used a gets even when submitting a form.
 end
